e1f35ee3fa5ad846be7dd1e84201ab837958c327
her15177 <her15177@uvg.edu.gt>
2022-11-20 14:54:16 -0600

    Add files via upload
    normal distribution and 15-30 secs

--- lab10prod.py
+++ lab10prod.py
@@ -1,37 +1,24 @@
 from confluent_kafka import Producer
 import json
-from faker import Faker
 import random
 from random import randint
+import numpy
+from time import sleep
 
 p = Producer({'bootstrap.servers': '147.182.206.35:9092'})
-
 puntos = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']
 
-
 def delivery_report(err, msg):
-    """ Called once for each message produced to indicate delivery result.
-        Triggered by poll() or flush(). """
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
         print('Message delivered to {} [{}]'.format(
             msg.topic(), msg.partition()))
 
-
-# for data in some_data_source:
 for _ in range(10):
-    data = {'temperatura': round(random.uniform(0, 100),2),    'humedad': randint(
-        0, 100), 'direccion': random.choice(puntos)}
+    sleep(randint(15,30))
+    data = {'temperatura': round(numpy.random.normal(50, 5),2),    'humedad':int(numpy.random.normal(50, 5)), 'direccion': random.choice(puntos)}
     print(data)
-    # Trigger any available delivery report callbacks from previous produce() calls
     p.poll(0)
-
-    # Asynchronously produce a message. The delivery report callback will
-    # be triggered from the call to poll() above, or flush() below, when the
-    # message has been successfully delivered or failed permanently.
     p.produce('lab10grupo10', json.dumps(data, indent=2).encode('utf-8'), callback=delivery_report)
-
-# Wait for any outstanding messages to be delivered and delivery report
-# callbacks to be triggered.
 p.flush()
